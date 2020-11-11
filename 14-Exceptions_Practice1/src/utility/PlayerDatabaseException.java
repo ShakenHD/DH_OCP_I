4e93d9784fe632fef33349da13acda1947565344
Daniel Hockley <danhockley1@gmail.com>
2020-11-11 15:16:45 +0200

    Chapter14

---
+++ 14-Exceptions_Practice1/src/utility/PlayerDatabaseException.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utility;
+
+/**
+ *
+ * @author danho
+ */
+public class PlayerDatabaseException extends Exception {
+    PlayerDatabaseException() {}
+    public PlayerDatabaseException(String message) {
+        super(message);
+    }
+}
